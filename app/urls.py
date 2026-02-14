05e9bfbee89615c70b168de3eae1e995373b7df7
JOHN <johnthomas.lm10@gmail.com>
2026-02-14 12:57:50 +0530

    tnt

---
+++ app/urls.py
@@ -0,0 +1,35 @@
+from django.urls import path,include
+from .import views
+urlpatterns = [
+
+    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('userhome/', views.userhome, name='userhome'),
+    path('workshopregister/', views.workshopregister, name='workshopregister'),
+
+
+    path('success/', views.success, name='success'),
+    path('adminlogin/',views.adminlogin,name='adminlogin'),
+    path('admin_dashboard/',views.admin_dashboard,name='admin_dashboard'),
+    path('users/', views.user_list, name='user_list'),
+    path('login/', views.login, name='login'),
+        path('wlogin/', views.wlogin, name='wlogin'),
+
+    path('forgotpassword/', views.forgotpassword, name='forgotpassword'),
+    path('verify-otp/', views.verify_otp_view, name='verify_otp'),
+    path('reset-password/', views.reset_password_view, name='reset_password'),  
+    path('workshoplist/', views.workshoplist, name='workshoplist'),  
+    path('register_vehicle/', views.register_vehicle, name='register_vehicle'), 
+    path('yourvehicles/', views.yourvehicles, name='yourvehicles'), 
+    path("book/", views.book_service, name="book_service"),
+
+    # path('logout/', views.logout_view, name='logout'),
+
+    
+
+
+
+    ]
