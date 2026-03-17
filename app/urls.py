d6263fb06ea88caf07859a9667ac06cc00111a49
JOHN <johnthomas.lm10@gmail.com>
2026-03-17 21:24:24 +0530

    quinton

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from .import views
 urlpatterns = [
 
-    path('', views.index, name='index'),
+    path('', views.login, name='login'),
     path('index/', views.index, name='index'),
 
     path('register/', views.register, name='register'),
